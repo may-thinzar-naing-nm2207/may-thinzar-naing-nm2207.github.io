e62d317cd672fae4d7894e5ca29e4dc55ceafaa3
may-thinzar-naing-nm2207 <125334852+may-thinzar-naing-nm2207@users.noreply.github.com>
2023-03-18 20:07:52 +0800

    Delete counter.js

--- counter.js
+++
@@ -1,8 +0,0 @@
-<script type='text/javascript'>    document.ondragstart = function () { return false; }; </script>
-<base target='_blank'/>
-
-<link href='http://media.tumblr.com/tumblr_m7efdhWLFX1qdlkyg.gif' rel='icon' type='image/x-icon'/>
-<title>Kawaii World ★</title>
-<link href='http://fonts.googleapis.com/css?family=Noticia+Text:700' rel='stylesheet' type='text/css'/>
-<link href='http://fonts.googleapis.com/css?family=Glass+Antiqua' rel='stylesheet' type='text/css'/>
-<link href='http://fonts.googleapis.com/css?family=Short+Stack' rel='stylesheet' type='text/css'/>
